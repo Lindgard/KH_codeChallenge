d784ccdc86971cf5f831af52fc6fe489f5381a91
ALP <a.b.lindgard@gmail.com>
2025-12-17 09:54:23 +0100

    Implementing some interactivity to eventually take advantage of the methods in Calculator.cs

--- Program.cs
+++ Program.cs
@@ -4,6 +4,14 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        //* Creating an instance of the calculator class
+        Calculator calc = new Calculator();
+
+        Console.WriteLine("Welcome to the calculator cli!");
+
+        //* Adding interactivity
+        Console.Write("Enter first number: ");
+        string input1 = Console.ReadLine();
+        int num1 = int.Parse(input1);
     }
 }
