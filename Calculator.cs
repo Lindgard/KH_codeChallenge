b0774c60af60da03a9990051066c48757ece86dc
ALP <a.b.lindgard@gmail.com>
2025-12-17 09:43:02 +0100

    added basic methods to the Calculator class

---
+++ Calculator.cs
@@ -0,0 +1,24 @@
+namespace codeChallenge;
+
+public class Calculator
+{
+    public int Add(int a, int b)
+    {
+        return a + b;
+    }
+
+    public int Subtract(int a, int b)
+    {
+        return a - b;
+    }
+
+    public int Multiply(int a, int b)
+    {
+        return a * b;
+    }
+
+    public int Divide(int a, int b)
+    {
+        return a / b;
+}
+}
